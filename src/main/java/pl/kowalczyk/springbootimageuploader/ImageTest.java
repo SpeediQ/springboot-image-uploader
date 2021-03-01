ffd098a9f2904f58f0a1569c0ebcf601616245c5
Marcin Kowalczyk <kowalczyk.m@int.pl>
2021-03-01 18:09:04 +0100

    Cloudinary added, getEnv added

--- src/main/java/pl/kowalczyk/springbootimageuploader/ImageTest.java
+++ src/main/java/pl/kowalczyk/springbootimageuploader/ImageTest.java
@@ -1,14 +1,33 @@
 package pl.kowalczyk.springbootimageuploader;
 
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 public class ImageTest {
-    public static void main(String[] args) {
+
+    private static final String cloudName = System.getenv("CLOUD_NAME");
+    private static final String apiKey = System.getenv("API_KEY");
+    private static final String api_secret = System.getenv("API_SECRET");
+
+    public static void main(String[] args) throws IOException {
+
+        photo();
 
 
+    }
 
+    private static void photo() throws IOException {
+        Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", api_secret));
 
-
-
+        File file = new File("D:\\Zdjęcia\\Grafika & Zdjęcia\\Marcin i Kasia\\Kasia\\DSC00729.JPG");
+        Map uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
     }
 }
